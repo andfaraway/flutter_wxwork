62bb397276d0dd4fb264730bbdac3841aa577fa4
libin <libin@yibujf.com>
2023-07-10 10:12:25 +0800

    first commit

--- lib/flutter_wxwork.dart
+++ lib/flutter_wxwork.dart
@@ -1,16 +1,62 @@
-import 'flutter_wxwork_method_channel.dart';
-import 'flutter_wxwork_platform_interface.dart';
+import 'dart:convert';
+import 'package:flutter/services.dart';
 
 class FlutterWxwork {
-  Future<bool> isAppInstalled() {
-    return FlutterWxworkPlatform.instance.isAppInstalled();
+  final methodChannel = const MethodChannel('flutter_wxwork');
+
+  Future<bool> isAppInstalled() async {
+    final result = await methodChannel.invokeMethod<String>('isAppInstalled');
+    return _stringToBool(result);
   }
 
-  Future<bool> register({required String scheme, required String corpId, required String agentId}) {
-    return FlutterWxworkPlatform.instance.register(scheme: scheme, corpId: corpId, agentId: agentId);
+  Future<bool> register({required String scheme, required String corpId, required String agentId}) async {
+    String? result = await methodChannel.invokeMethod<String>('register', <String, String>{
+      'scheme': scheme,
+      'corpId': corpId,
+      'agentId': agentId,
+    });
+    return _stringToBool(result);
   }
 
   Future<AuthModel> auth({String state = 'state'}) async {
-    return FlutterWxworkPlatform.instance.auth(state: state);
+    var content = await methodChannel.invokeMethod('auth', {"state": state});
+    return AuthModel.fromJson(Map<String, dynamic>.from(content));
+  }
+
+  bool _stringToBool(String? string) {
+    if (string == '1') {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+}
+
+
+
+class AuthModel {
+  /// 1.取消 0.成功 2.失败
+  String? errCode;
+  String? code;
+  String? state;
+
+  AuthModel();
+
+  factory AuthModel.fromJson(Map<String, dynamic> json) {
+    AuthModel model = AuthModel();
+    model.errCode = json['errCode'];
+    model.code = json['errCode'];
+    model.state = json['state'];
+    return model;
+  }
+
+  Map<String, dynamic> toJson() {
+    return {'errorCode': errCode, 'code': code, 'state': state};
+  }
+
+  @override
+  String toString() {
+    return jsonEncode(this);
   }
 }
